21aab00e8e76e6588c6bf0a7c88d2e3978d965f4
evan Dangol <dangolevan@gmail.com>
2021-08-22 22:02:42 +0545

    readm

---
+++ EvanDangolStandard/Validation/EvanValidation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EvanDangol.Validation
+{
+    class EvanValidation
+    {
+    }
+    public class MailManager
+    {
+        public bool IsValidAddress(string emailAddress)
+        {
+            Regex regex = new Regex(@"^[\w0-9._%+-]+@[\w0-9.-]+\.[\w]{2,6}$");
+            return regex.IsMatch(emailAddress);
+        }
+    }
+}
